f647b4fc101ed2a0fdbe3eba306f3d61fffc5f8a
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-21 00:55:22 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
   # your code here
+  sqarray = Array.new
+  counter = 0
+  while array[counter] do
+    sqarray.push(array[counter] ** 2)
+    counter += 1
+end
+sqarray
 end
